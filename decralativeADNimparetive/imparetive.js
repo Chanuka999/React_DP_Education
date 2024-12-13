42b3ea92746cadec7ce58c1dc939eb87e332de43
Chanuka999 <chanukaranditha99@gmail.com>
2024-12-14 01:31:15 +0530

    imparative.js

---
+++ decralativeADNimparetive/imparetive.js
@@ -0,0 +1,9 @@
+const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
+const evenNumbers = [];
+
+for(let i=0;i<numbers.length;i++){
+    if(numbers[i]%2 ===0){
+        evenNumbers.push(numbers[i]);
+    }
+}
+console.log(evenNumbers);
