e0d4c3fd2acc942dbce832243ed481ca0dd6d415
Chanuka999 <chanukaranditha99@gmail.com>
2024-12-14 01:27:41 +0530

    sync and async lessons

---
+++ syncAndAsync/Async.js
@@ -0,0 +1,17 @@
+function OneToFive(){
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(4);
+    console.log(5);
+    
+}
+console.log("start");
+
+setTimeout(OneToFive,3000);
+
+console.log(6);
+console.log(7);
+console.log(8);
+console.log(9);
+console.log(10);
