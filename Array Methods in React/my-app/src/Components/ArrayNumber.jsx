d7a98870fc79e2248452ee7ad7573181bca16a10
Chanuka999 <chanukaranditha99@gmail.com>
2024-12-20 17:25:13 +0530

    reduce method learning

---
+++ Array Methods in React/my-app/src/Components/ArrayNumber.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ArrayNumber = ({ numberList }) => {
+  const sum = numberList.reduce((total, num) => total + num);
+  return (
+    <div>
+      <h1>Number List</h1>
+      <ul>
+        {numberList.map((number, index) => (
+          <li key={index}>{number}</li>
+        ))}
+      </ul>
+      <hr />
+      <p>{sum}</p>
+    </div>
+  );
+};
+
+export default ArrayNumber;
