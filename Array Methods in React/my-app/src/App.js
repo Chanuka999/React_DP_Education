e6d7bf1287b6990b64cab311807d48cb6c047585
Chanuka999 <chanukaranditha99@gmail.com>
2024-12-20 17:24:38 +0530

    update

--- Array Methods in React/my-app/src/App.js
+++ Array Methods in React/my-app/src/App.js
@@ -1,6 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Arraylist from "./Components/Arralist";
+import ArrayFilter from "./Components/ArrayFilter";
+import ArrayNumber from "./Components/ArrayNumber";
 
 function App() {
   const name = [
@@ -18,11 +20,15 @@ function App() {
     "fransiska",
     "kathrina",
   ];
+
+  const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Arraylist nameList={name} />
+        <ArrayFilter nameList={name} />
+        <ArrayNumber numberList={number} />
       </header>
     </div>
   );
