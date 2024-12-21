5fa229a6ebf4e8f49add14a9f38c7f27f6b2cb15
Chanuka999 <chanukaranditha99@gmail.com>
2024-12-22 01:08:52 +0530

    fregment method learnning

--- Array Fregment/my-app/src/Components/ListItem.jsx
+++ Array Fregment/my-app/src/Components/ListItem.jsx
@@ -1,10 +1,10 @@
 const ListItem = ({ items }) => {
   return (
-    <div>
+    <>
       {items.map((item, index) => (
         <li key={index}>{item}</li>
       ))}
-    </div>
+    </>
   );
 };
 
