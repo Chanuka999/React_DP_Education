293e9da5c4272d32bc8a01784ededb42f0abd28e
Chanuka999 <chanukaranditha99@gmail.com>
2024-12-21 23:16:49 +0530

    add app.js

---
+++ Array Fregment/my-app/src/App.js
@@ -0,0 +1,30 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const name = [
+    "jonas",
+    "martha",
+    "mickel",
+    "chalert",
+    "hennah",
+    "elisabath",
+    "peter",
+    "clodia",
+    "adam",
+    "iwa",
+    "silja",
+    "fransiska",
+    "kathrina",
+  ];
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
