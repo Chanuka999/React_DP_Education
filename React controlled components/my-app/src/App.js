15b316d4aadafd15fdfcf2275bf3b5b1181e47c8
Chanuka999 <chanukaranditha99@gmail.com>
2024-12-23 00:55:47 +0530

    added

---
+++ React controlled components/my-app/src/App.js
@@ -0,0 +1,34 @@
+import logo from "./logo.svg";
+import "./App.css";
+import OrderdListItem from "./Components/OrderdListItem";
+import UnorderdListItem from "./Components/UnorderdListItem";
+
+function App() {
+  const nameList = [
+    "jonas",
+    "martha",
+    "mickel",
+    "chalert",
+    "hennah",
+    "elisabath",
+    "peter",
+    "clodia",
+    "adam",
+    "iwa",
+    "silja",
+    "fransiska",
+    "kathrina",
+  ];
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <OrderdListItem itemList={nameList} />
+        <UnorderdListItem itemList={nameList} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
