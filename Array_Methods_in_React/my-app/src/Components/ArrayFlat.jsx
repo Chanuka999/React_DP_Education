abc229a3d52b662d4ad67c7bc6f929dd7e5c8cb8
Chanuka999 <chanukaranditha99@gmail.com>
2025-04-11 20:17:40 +0530

    update

--- Array_Methods_in_React/my-app/src/Components/ArrayFlat.jsx
+++ Array_Methods_in_React/my-app/src/Components/ArrayFlat.jsx
@@ -1,4 +1,37 @@
-const ArrayFlat = () => {
-  <div></div>;
+const ArrayFlat = ({ nameList }) => {
+  const newArray = [
+    ...nameList.slice(0, 3),
+    [nameList[3], nameList[4], nameList.slice(5, nameList.length)],
+  ];
+
+  const arr1 = newArray.flat();
+  const arr2 = newArray.flat(2);
+  return (
+    <div>
+      <h1>Array Flat</h1>
+
+      <table border={1} cellPadding={5}>
+        <tr>
+          <td>{JSON.stringify(newArray)}</td>
+        </tr>
+      </table>
+
+      <h1>Array Flat 1</h1>
+
+      <table border={1} cellPadding={5}>
+        <tr>
+          <td>{JSON.stringify(arr1)}</td>
+        </tr>
+      </table>
+
+      <h1>Array Flat 2</h1>
+
+      <table border={1} cellPadding={5}>
+        <tr>
+          <td>{JSON.stringify(arr2)}</td>
+        </tr>
+      </table>
+    </div>
+  );
 };
 export default ArrayFlat;
