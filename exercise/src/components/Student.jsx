c76b188e94b0bd4d0c2f83f3d4b05eeb388cd8c6
Chanuka999 <chanukaranditha99@gmail.com>
2025-12-26 20:55:07 +0530

    add to event handler for age change

--- exercise/src/components/Student.jsx
+++ exercise/src/components/Student.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Student = ({ name, age, city }) => {
+  const [newAge, setNewAge] = useState(age);
+
+  const handleWheel = (event) => {
+    console.log(event.deltaY);
+    if (event.deltaY > 0) {
+      console.log("move to down");
+      setNewAge(newAge + 1);
+    } else {
+      console.log("move to up");
+      setNewAge(newAge - 1);
+    }
+  };
+
   return (
     <div>
       <p>{name}</p>
-      <p>{age}</p>
+      <p onWheel={handleWheel}>{newAge}</p>
       <p>{city}</p>
     </div>
   );
