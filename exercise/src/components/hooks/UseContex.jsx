275c36d2ad7f797a7ca9574354e55314eff4a141
Chanuka999 <chanukaranditha99@gmail.com>
2025-12-30 14:21:45 +0530

    Add UseContex component to demonstrate context usage with BoxA

---
+++ exercise/src/components/hooks/UseContex.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import BoxA from "./BoxA";
+
+const UseContex = () => {
+  return (
+    <div>
+      <h1>Use context</h1>
+      <BoxA />
+    </div>
+  );
+};
+
+export default UseContex;
